fe6e5ef6d430d90d3400832eb8afd09ae8139a72
Miroslav Grund <miroslavgrund68@gmail.com>
2025-06-09 11:57:12 +0200

    Person and Movie detail

---
+++ client/src/movies/MovieDetail.jsx
@@ -0,0 +1,60 @@
+import React, { act, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { apiGet } from "../utils/api";
+import Genre from "./Genre";
+
+const MovieDetail = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    apiGet("/api/movies/" + id)
+      .then((data) => {
+        setMovie({
+          name: data.name,
+          year: data.year,
+          director: data.director,
+          actors: data.actors,
+          genres: data.genres,
+          isAvailable: data.isAvailable,
+          dateAdded: data.dateAdded,
+        });
+      })
+      .catch(
+        (error) => {
+          console.log(error);
+        },
+        [id]
+      );
+  }, [id]);
+
+  const genres = movie.genres?.map((item) => Genre[item]);
+  const actors = movie.actors?.map((item) => item.name);
+  const dateAdded = new Date(movie.dateAdded).toLocaleString();
+
+  return (
+    <div>
+      <h1>Detail filmu</h1>
+      <hr />
+      <h3>
+        {movie.name} <small>({movie.year})</small>
+      </h3>
+      <p>{genres?.join(" / ")}</p>
+      <p>
+        <strong>Režie: </strong>
+        {movie.director?.name}
+        <br />
+        <strong>Hrají:</strong>
+        {actors?.join(", ")}
+        <br />
+        <strong>Dostupný: </strong>
+        {movie.isAvailable ? "ANO" : "NE"}
+        <br />
+        <em>Vytvořeno {dateAdded}</em>
+      </p>
+    </div>
+  );
+};
+
+export default MovieDetail;
---
+++ client/src/persons/PersonDetail.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { apiGet } from "../utils/api";
+import { dateStringFormatter } from "../utils/dateStringFormatter";
+import Role from "./Role";
+
+const PersonDetail = () => {
+  const { id } = useParams();
+  const [person, setPerson] = useState({});
+
+  useEffect(() => {
+    apiGet("/api/people/" + id)
+      .then((data) => {
+        setPerson(
+          {
+            name: data.name,
+            birthDate: dateStringFormatter(data.birthDate, true),
+            country: data.country,
+            biography: data.biography,
+            role: data.role,
+          }.cat
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
+  const role = Role.DIRECTOR === person.role ? "Režisér" : "Herec";
+
+  return (
+    <div>
+      <h1>Detail osobnosti</h1>
+      <hr />
+      <h3>{person.name}</h3>
+      <p>
+        {role}, nar. {person.birthDate}, {person.country}
+      </p>
+      <p>
+        <strong>Biografie:</strong>
+        <br />
+        {person.biography}
+      </p>
+    </div>
+  );
+};
+
+export default PersonDetail;
